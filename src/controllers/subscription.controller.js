0dd1783aa62dc7304e4d0dde3b3bc9229d334ae9
Kamran <kamran.khan.03112002@gmail.com>
2024-08-28 03:15:14 +0530

    Added subscription model and controllers

---
+++ src/controllers/subscription.controller.js
@@ -0,0 +1,138 @@
+import mongoose, {isValidObjectId} from "mongoose"
+import {User} from "../models/user.model.js"
+import { Subscription } from "../models/subscription.model.js"
+import {ApiError} from "../utils/ApiError.js"
+import {ApiResponse} from "../utils/ApiResponse.js"
+import {asyncHandler} from "../utils/asyncHandler.js"
+
+
+const toggleSubscription = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+    const userId=req.user?._id
+
+    if(!(channelId && userId)){
+        throw new ApiError(400,"Error while fetching the required data for toggling subscription")
+    }
+   
+    const subscription=await Subscription.findOne({channel:channelId, subscriber:userId})
+
+    if(!subscription){
+        await Subscription.findByIdAndDelete(subscription._id,{new:true})
+    }
+    else{
+        await Subscription.create({channel:channelId,subscriber:userId})
+    }
+
+    return res.status(201).json(new ApiResponse(200,subscription,"Subscription toggle successful"))
+    
+    // TODO: toggle subscription
+})
+
+// controller to return subscriber list of a channel
+const getUserChannelSubscribers = asyncHandler(async (req, res) => {
+    const {channelId} = req.params
+    if(!channelId){
+        throw new ApiError(400,"Error while fetching the channel id")
+    }
+
+    if(!req.user?._id){
+        throw new ApiError(400,"Unauthorized request")
+    }
+
+    if(channelId!==req.user?._id){
+        throw new ApiError(404,"This channel is not owned by you")
+    }
+
+    const subscription=await Subscription.aggregate([
+        {
+            $match:{
+                channel:new mongoose.Types.ObjectId(channelId)
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"subscriber",
+                foreignField:"_id",
+                as:"subscriber",
+                pipeline:[
+                    {
+                        $project:{
+                            username:1,
+                            fullname:1,
+                            avatar:1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields:{
+                subscriber:{
+                    $first:"$subscriber"
+                }
+            }
+        }
+    ])
+
+    if(!subscription){
+        throw new ApiError(400,"Error encountered while fetching subscribers list")
+    }
+
+    return res.status(201).json(new ApiResponse(200,subscription,"Subscribers list fetched successfully"))
+
+})
+
+// controller to return channel list to which user has subscribed
+const getSubscribedChannels = asyncHandler(async (req, res) => {
+    const { subscriberId } = req.params
+
+    if(!subscriberId){
+        throw new ApiError(400,"Error while fetching the channel id")
+    }
+
+    const channel=await Subscription.aggregate([
+        {
+            $match:{
+                subscriber:new mongoose.Types.ObjectId(subscriberId)
+            }
+        },
+        {
+            $lookup:{
+                from:"users",
+                localField:"channel",
+                foreignField:"_id",
+                as:"channel",
+                pipeline:[
+                    {
+                        $project:{
+                            username:1,
+                            fullname:1,
+                            avatar:1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields:{
+                subscriber:{
+                    $first:"$channel"
+                }
+            }
+        }
+    ])
+
+    if(!channel){
+        throw new ApiError(400,"Error encountered while fetching channel list")
+    }
+
+    return res.status(201).json(new ApiResponse(200,channel,"Channel list fetched successfully"))
+
+})
+
+export {
+    toggleSubscription,
+    getUserChannelSubscribers,
+    getSubscribedChannels
+}
